1f30302f9566ac22cfbe677ca88b9c86afe05af5
PickHD <taufikjanuar35@gmail.com>
2021-05-06 19:54:59 +0700

    Feat : Add redis into UserBaseHandler

--- src/apps/users/user.base.go
+++ src/apps/users/user.base.go
@@ -1,13 +1,18 @@
 package users
 
-import "gorm.io/gorm"
+import (
+  "gorm.io/gorm"
+  "github.com/go-redis/redis/v8"
+)
 
 type UserBaseHandler struct {
   DB *gorm.DB
+  rdsDB *redis.Client
 }
 
-func NewUserBaseHandler(db *gorm.DB) *UserBaseHandler {
+func NewUserBaseHandler(db *gorm.DB,rds *redis.Client) *UserBaseHandler {
   return &UserBaseHandler{
     DB:db,
+    rdsDB: rds,
   }
 }
